7a12ec55a57e65a9d151ebca9f25bac36e2d5f8c
huxingtao <36324488+ibule@users.noreply.github.com>
2023-08-26 16:29:16 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+
 echo -e "---------docker Login--------"
-docker login haomingkeji-docker.pkg.coding.net --username=$1  --password=$2
+docker login haomingkeji-docker.pkg.coding.net --username=$1  --password=$2 || exit 1
+
 echo -e "---------docker Stop--------"
 docker stop nobelium
+
 echo -e "---------docker Rm--------"
 docker rm nobelium && docker rmi haomingkeji-docker.pkg.coding.net/zaixiankaoshi/public/ibule/nobelium:latest
+
 echo -e "---------docker Pull--------"
-docker pull haomingkeji-docker.pkg.coding.net/zaixiankaoshi/public/ibule/nobelium:latest
+docker pull haomingkeji-docker.pkg.coding.net/zaixiankaoshi/public/ibule/nobelium:latest || exit 1
+
 echo -e "---------docker Create and Start--------"
-docker run --restart=always --rm -d -p 3000:3000 --name nobelium haomingkeji-docker.pkg.coding.net/zaixiankaoshi/public/ibule/nobelium:latest
+docker run --restart=always -d -p 3000:3000 --name nobelium haomingkeji-docker.pkg.coding.net/zaixiankaoshi/public/ibule/nobelium:latest || exit 1
+
 echo -e "---------deploy Success--------"
